d75db74f22213df5fc874730a271e1ab028f741f
nw1709 <110033749+nw1709@users.noreply.github.com>
2025-06-20 01:20:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def validate_keys():
 validate_keys()
 
 # --- UI-Einstellungen ---
-st.set_page_config(layout="centered", page_title="Koifox-Bot", page_icon="🦊")
+st.set_page_config(layout="centered", page_title="3.0", page_icon="🌕")
 st.title("🦊 Koifox-Bot")
 st.markdown("*Verbesserte OCR für Graphen, strikte Formatierung & Konsistenzprüfung*")
 
